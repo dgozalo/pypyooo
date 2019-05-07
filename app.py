7bba607150a10dd24db6086158b6585adfb79851
Daniel Gozalo <dgozalo@cloudbees.com>
2019-05-07 14:29:43 +0200

    change 2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ class MyHandler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     """Handler for GET requests"""
-    self.send_response(200)
+    self.send_response(204)
     self.send_header('Content-type','image/png')
     self.end_headers()
     with open('logo.png', 'rb') as f:
